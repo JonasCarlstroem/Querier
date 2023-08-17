ec98b77e98531b9898d29aab50cf2a08e8eaafc9
JonasCarlstroem <82809067+JonasCarlstroem@users.noreply.github.com>
2023-08-18 00:01:11 +0200

    Update App.cpp
    Todos

--- App.cpp
+++ App.cpp
@@ -3,6 +3,22 @@
 
 #include "App.h"
 
+//####Todo####
+//###General###
+// base module class (abstract)
+// module handler
+// support for multiple queries (mod handler)
+// integrate http library
+//
+//###Node###
+// make module class out of current state of this class then make this class a module
+// implement npm with gui
+// implement version checking (including check to see if nvm is installed)
+// 
+//###CSharp###
+// imolement as module
+// split module into type of either interpreter or compiler
+
 using json = nlohmann::json;
 
 int WINAPI WinMain(
